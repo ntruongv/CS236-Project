9d867a3f2a0675a2f5ffb48067a072e1f81f5e77
Dan Johnson <dansj@stanford.edu>
2019-12-01 01:54:41 +0000

    update runs

--- methods/1_sgan/train.sh
+++ methods/1_sgan/train.sh
@@ -5,5 +5,5 @@
 #SBATCH --time=4:25:00
 
 cd Code
-echo $PWD > ~/.conda/envs/socialgan/lib/python3.5/site-packages/sgan.pth
+echo $PWD > ~/.conda/envs/socialgan_with_torch1.3.1/lib/python3.5/site-packages/sgan.pth
 python scripts/train.py --output_dir ../checkpoints --dataset_name crowds_zara --restore_from_checkpoint 1 --l2_loss_weight 1.0
--- methods/5_attention/Code/sgan/models_w_local_context.py
+++ methods/5_attention/Code/sgan/models_w_local_context.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import os
+import math
+import torch.nn.functional as F
+import torch.nn.modules.activation as act
 
 import sys
 codepath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -33,6 +36,32 @@ def get_noise(shape, noise_type):
         return torch.rand(*shape).sub_(0.5).mul_(2.0).cuda()
     raise ValueError('Unrecognized noise type "%s"' % noise_type)
 
+class EncoderAtt(nn.Module):
+    def __init__(self, embedding_dim=64, h_dim=64, mlp_dim=1024, num_layers=1, dropout=0.0):
+        super(EncoderAtt, self).__init__()
+
+        self.mlp_dim = mlp_dim
+        self.h_dim = h_dim
+        self.embedding_dim = embedding_dim
+        self.num_layers = num_layers
+
+        # self.self_attention_norm = nn.LayerNorm(embedding_dim, eps=1e-6)
+        self.self_attention = act.MultiheadAttention(embedding_dim, 8, dropout=dropout)
+        # self.self_attention_dropout = nn.Dropout(dropout)
+
+        self.spatial_embedding = nn.Linear(2, embedding_dim)
+
+    def forward(self, obs_traj):  # pylint: disable=arguments-differ
+        batch = obs_traj.size(1)
+        obs_traj_embedding = self.spatial_embedding(obs_traj.view(-1, 2))
+        obs_traj_embedding = obs_traj_embedding.view(
+            -1, batch, self.embedding_dim
+        )
+        
+        attn_output, attn_output_weights = self.self_attention(obs_traj_embedding, obs_traj_embedding, obs_traj_embedding)
+        # feat_h = torch.sum(attn_output, dim=0, keepdims=True)
+        feat_h = attn_output[-1].unsqueeze(0)
+        return feat_h
 
 class Encoder(nn.Module):
     """Encoder is part of both TrajectoryGenerator and
@@ -389,7 +418,7 @@ class TrajectoryGenerator(nn.Module):
         self.pool_every_timestep = pool_every_timestep
         self.bottleneck_dim = 1024
 
-        self.encoder = Encoder(
+        self.encoder = EncoderAtt(
             embedding_dim=embedding_dim,
             h_dim=encoder_h_dim,
             mlp_dim=mlp_dim,
@@ -579,7 +608,7 @@ class TrajectoryDiscriminator(nn.Module):
         self.h_dim = h_dim
         self.d_type = d_type
 
-        self.encoder = Encoder(
+        self.encoder = EncoderAtt(
             embedding_dim=embedding_dim,
             h_dim=h_dim,
             mlp_dim=mlp_dim,
