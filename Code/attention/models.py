0b84b9a6edc1db18a3dd9b8d80b8bb5e2c44f937
Shubh Gupta <shubhg1996@gmail.com>
2019-12-09 13:44:24 -0800

    discriminator mean

--- Code/attention/models.py
+++ Code/attention/models.py
@@ -121,4 +121,5 @@ class TrajectoryDiscriminatorPiecewise(nn.Module):
         else:
             raise NotImplementedError
         scores = self.real_classifier(classifier_input)
+        scores = scores.view(batch, -1).mean(dim=1)
         return scores
